171f12bb85f96caec812e86378ba5496a00bdc8a
Ismael <brandon.freire@epn.edu.ec>
2023-11-21 18:28:29 -0500

    version codigo 1.1

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -11,8 +11,24 @@
 */
 
 public class Ejercicio5 {
+    int primerNum = 2;
+    int contador=0;
 
-    public void Ejercicio5For (int ){
-        Scanner scanner = new ;
+    public void Ejercicio5For (int nPosiciones){
+        for(int i=primerNum; i < nPosiciones; i++){
+            if (esPrimo(nPosiciones)) {
+                System.out.println(i);
+            }
+        }
+    }
+    public boolean esPrimo(int numero){
+        int aux;
+        for(int contador=primerNum; contador<numero; contador++){
+            aux=numero%contador;
+            if (aux==0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
