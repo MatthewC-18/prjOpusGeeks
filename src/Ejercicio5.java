f241d100571cb0ec8c4f3f4614c3c7a9ccd49340
Ismael <brandon.freire@epn.edu.ec>
2023-11-21 18:54:56 -0500

    Correcciones al codigo

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -13,17 +13,31 @@
 public class Ejercicio5 {
     int primerNum = 2;
     int contador=0;
-
+    /**
+     * Se inicializa el constructor para que con el parametro nPosiciones detecte e imprima los numeros primos 
+     * @param nPosiciones
+     */
     public void Ejercicio5For (int nPosiciones){
+        /**
+         * El ciclo for recorrera desde el 2 hasta el numero ingresado por consola
+         */
         for(int i=primerNum; i < nPosiciones; i++){
+            /**
+             * Luego el if determinará si es un numero primo y lo imprimirá 
+             */
             if (esPrimo(nPosiciones)) {
                 System.out.println(i);
             }
         }
     }
+    /**
+     * 
+     * @param numero
+     * @return
+     */
     public boolean esPrimo(int numero){
         int aux;
-        for(int contador=primerNum; contador<numero; contador++){
+        for(int contador=primerNum; contador<numero-1; contador++){
             aux=numero%contador;
             if (aux==0) {
                 return false;
