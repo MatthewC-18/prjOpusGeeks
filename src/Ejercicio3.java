e12713fb6ec4c2b5a4c900b0e5d4afacbbe18fc2
freddy1604 <freddyjimenez2003@gmail.com>
2023-11-21 14:36:47 -0500

    Update Ejercicio3.java

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,12 +1,37 @@
-public class Ejercicio3 {
+/*
+*Copyright (C) 2K23, grupo2
+*(grupo2 - Opus Geeks), freddyjimenez2003@gmail.com
+*Version 1.0.
+*AUTOR: FREDDY JIMENEZ.
+*FECHA: 21/11/2023.
+*/
+
+
+/**
+*Este codigo utiliza un bucle llamado "for" para imprimir una serie de fracciones en formato "numerador/denominador".
+*/
+public class Ejercicio3 {                                                    
     public void Ejercicio3For (int nPosiciones){
         int numerador1   = 0;
         int numerador2   = 1;
         int denominador1 = 1;
         int salto = 2;
+/**
+*Este bucle se ejecutara "nPosiciones" veces.
+*/
         for(int i = 0; i < nPosiciones; i++){
+/**
+*Imprime el numerador1 seguido de "/".
+*Imprime el denominador1 seguido de un espacio.
+*/
             System.out.print(numerador1+ "/");
             System.out.print(denominador1+ " ");
+/**
+*Calcula la suma de los dos numeradores y almacena el resultado en signumerador.
+*Actualiza numerador1 con el valor actual de numerador2.
+*Actualiza numerador2 con el nuevo valor calculado.
+*Aumenta el denominador1 sumándole el valor de salto.
+*/
             int signumerador = numerador1 + numerador2;
             numerador1 = numerador2;
             numerador2 = signumerador;
