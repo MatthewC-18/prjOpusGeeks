61c204a4852423103480d8933a262e54d8ff377d
Matthew <matthewcedeno92@gmail.com>
2023-11-21 10:38:43 -0600

    Primera serie

--- src/Ejemplo1.java
+++
@@ -1,8 +0,0 @@
-public class Ejemplo1 {
-    public static void main(String[] args) {
-        // Utilizamos un bucle for para imprimir los primeros 10 números
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-    }
-}
---
+++ src/Ejercicio1.java
@@ -0,0 +1,19 @@
+/*Copyrigh  (C) 
+ * matthewcedeno92@gmail.com
+ * Version 1.0
+ */
+/*
+ * @author
+ */
+public class Ejercicio1 {
+    /*
+     * @param Nroterminos: numero de terminos para la serie
+     * @version 1.0
+     */
+    public void showSerie(int Nroterminos){
+        for (int i = 0; i < Nroterminos; i++) {
+            System.out.println(i);
+        }
+
+    }
+}   
