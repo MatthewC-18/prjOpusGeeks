c0302da95de96f6eeb8c41e4570246bddc05fa40
isabel <151420342+y-iss@users.noreply.github.com>
2023-11-23 11:52:44 -0500

    Ejercicio9.java

--- src/Ejercicio9.java
+++ src/Ejercicio9.java
@@ -4,12 +4,10 @@
 * Isabel Farinango 
   */
 
-
 public class Ejercicio9 {
 
   public static void main(String[] args) {
     serieS9();
-    serieS10();
   }
 //Funcion que imprime la serie S9
 public static void serieS9() {
@@ -20,24 +18,9 @@ public static void serieS9() {
     for (int i = 1; i <= 8; i++) {
       System.out.println(resultado + ",");
       resultado *= 2; 
-  }
-  System.out.println(); 
-}
-  //Funcion que imprime la serie S10
-public static void serieS10() {
-  System.out.println("S10: ");
-  int resultado = 3; 
-  int exponente = 1; 
-    //Bucle while
-    while (exponente<=7) {
-      System.out.println(resultado + ",");
-      resultado *= 3;
-      exponente++;
-      
     }
-    System.out.println();
-  } 
-
+  System.out.println(); 
+  }
 }
 
 
