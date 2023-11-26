889c738100e0b53c9b1ee4c6db6160aa59786ff3
isabel <151420342+y-iss@users.noreply.github.com>
2023-11-25 21:09:11 -0500

    CorreccionEjercicio10

---
+++ src/Ejercicio10.java
@@ -0,0 +1,54 @@
+/** 
+*Copyright (C)
+* (grupo 2 - OpusGeeks )alegria.farinango@epn.edu.ec 
+* Isabel Farinango 
+  */
+
+public class Ejercicio10 {
+
+  public static void main(String[] args) {
+    int nPosiciones=7;
+    serieS10For(nPosiciones); 
+    serieS10While(nPosiciones); 
+    serieS10DoWhile(nPosiciones); 
+}
+
+/*Funcion imprime S10, ocupando el bucle for */
+public static void serieS10For(int nPosiciones){
+  System.out.println("Serie S10 ocupando for: ");
+int resultado=3; 
+for(int exponente=1; exponente<=nPosiciones; exponente++){
+  System.out.print(resultado + " , ");
+  resultado *=3;  
+  }
+  System.out.println();
+
+}
+
+/*Funcion imprime S10, ocupando el bucle while */
+public static void serieS10While(int nPosiciones){
+  System.out.println("Serie S10 ocupando while: ");
+  int resultado=3; 
+  int exponente=1;
+  while(exponente<=nPosiciones){
+    System.out.print(resultado + " , ");
+    resultado *=3;  
+    exponente++;
+  }
+  System.out.println();
+}
+/*Funcion imprime S10, ocupando el bucle do-while */
+public static void serieS10DoWhile(int nPosiciones){
+  System.out.println("Serie S10 ocupando do-while: ");
+  int resultado=3; 
+  int exponente=1;
+  do {
+    System.out.print(resultado + " , ");
+    resultado *=3;  
+    exponente++;
+  }while(exponente<=nPosiciones);
+  System.out.println();
+  } 
+
+}
+
