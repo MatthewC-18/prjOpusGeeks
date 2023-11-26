7f368eee015ad13c22dde6f0ec901f6be123883b
isabel <151420342+y-iss@users.noreply.github.com>
2023-11-25 21:41:51 -0500

    CorrecionEjercicio10.java

--- src/Ejercicio10.java
+++ src/Ejercicio10.java
@@ -14,7 +14,7 @@ public class Ejercicio10 {
 }
 
 /*Funcion imprime S10, ocupando el bucle for */
-public static void serieS10For(int nPosiciones){
+public static void Ejercicio10For(int nPosiciones){
   System.out.println("Serie S10 ocupando for: ");
 int resultado=3; 
 for(int exponente=1; exponente<=nPosiciones; exponente++){
@@ -26,7 +26,7 @@ for(int exponente=1; exponente<=nPosiciones; exponente++){
 }
 
 /*Funcion imprime S10, ocupando el bucle while */
-public static void serieS10While(int nPosiciones){
+public static void Ejercicio10While(int nPosiciones){
   System.out.println("Serie S10 ocupando while: ");
   int resultado=3; 
   int exponente=1;
@@ -38,7 +38,7 @@ public static void serieS10While(int nPosiciones){
   System.out.println();
 }
 /*Funcion imprime S10, ocupando el bucle do-while */
-public static void serieS10DoWhile(int nPosiciones){
+public static void Ejercicio10DoWhile(int nPosiciones){
   System.out.println("Serie S10 ocupando do-while: ");
   int resultado=3; 
   int exponente=1;
