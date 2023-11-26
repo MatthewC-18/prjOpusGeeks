56224457d27ecd6458468b3723d20939cbf02022
isabel <151420342+y-iss@users.noreply.github.com>
2023-11-26 16:34:47 -0500

    Ejercicio9.java

--- src/Ejercicio9.java
+++ src/Ejercicio9.java
@@ -6,38 +6,45 @@
 
 public class Ejercicio9 {
 
+  public static void main(String[] args) {
+    int nPosiciones=8;
+    Ejercicio9For(nPosiciones); 
+    Ejercicio9DoWhile(nPosiciones); 
+    Ejercicio9While(nPosiciones); 
+   
+  }
   /*Funcion imprime S9, ocupando el bucle for */
-  public void Ejercicio9For(int nPosiciones) {
+  public static void Ejercicio9For(int nPosiciones) {
     System.out.println("serie S9 ocupando for: ");
     int resultado=2; 
     for (int i=1; i<=nPosiciones; i++) {
       System.out.print(resultado + " ");
-      resultado+=2;
+      resultado*=2;
     }
     System.out.println();
   }
 
   /*Funcion imprime S9, ocupando el bucle do-while */
-  public void Ejercicio9DoWhile(int nPosiciones) {
+  public static void Ejercicio9DoWhile(int nPosiciones) {
     System.out.println("serie S9 ocupando do-while: ");
     int resultado=2;
     int i=1;
     do {
       System.out.print(resultado + " ");
-      resultado+=2;
+      resultado*=2;
       i++;
     } while (i<=nPosiciones);
     System.out.println();
   }
  
   /*Funcion imprime S9, ocupando el bucle while */
-  public void Ejercicio9While(int nPosiciones) {
+  public static void Ejercicio9While(int nPosiciones) {
     System.out.println("serie S9 ocupando while: ");
     int resultado=2; 
     int i=1;
     while (i<=nPosiciones) {
       System.out.print(resultado + " ");
-      resultado+=2;
+      resultado*=2;
       i++;
     }
     System.out.println();
