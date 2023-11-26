7391a7b2848f4c503e90b7e8ed95256a87da26f5
isabel <151420342+y-iss@users.noreply.github.com>
2023-11-25 21:08:30 -0500

    CorrecionEjercicio9

---
+++ src/Ejercicio9.java
@@ -0,0 +1,55 @@
+/** 
+*Copyright (C)
+* (grupo 2 - OpusGeeks )alegria.farinango@epn.edu.ec 
+* Isabel Farinango 
+  */
+
+public class Ejercicio9 {
+
+  public static void main(String[] args) {
+    int nPosiciones=8;
+    serieS9For(nPosiciones); 
+    serieS9While(nPosiciones); 
+    serieS9DoWhile(nPosiciones); 
+  }
+  /*Funcion imprime S9, ocupando el bucle for */
+  public static void serieS9For(int nPosiciones) {
+    System.out.println("serie S9 ocupando for: ");
+    int resultado=2; 
+    for (int i=1; i<=nPosiciones; i++) {
+      System.out.print(resultado + " ");
+      resultado+=2;
+    }
+    System.out.println();
+  }
+  /*Funcion imprime S9, ocupando el bucle while */
+  public static void serieS9While(int nPosiciones) {
+    System.out.println("serie S9 ocupando while: ");
+    int resultado=2; 
+    int i=1;
+    while (i<=nPosiciones) {
+      System.out.print(resultado + " ");
+      resultado+=2;
+      i++;
+    }
+    System.out.println();
+  }
+  /*Funcion imprime S9, ocupando el bucle do-while */
+  public static void serieS9DoWhile(int nPosiciones) {
+    System.out.println("serie S9 ocupando do-while: ");
+    int resultado=2;
+    int i=1;
+    do {
+      System.out.print(resultado + " ");
+      resultado+=2;
+      i++;
+    } while (i<=nPosiciones);
+    System.out.println();
+  }
+}
+
+
+  
+
+
+
