ba88789227678f728aaac83b92cdb2ec5ee7b9d7
Ismael <brandon.freire@epn.edu.ec>
2023-12-09 16:46:44 -0500

    cambios menu

--- src/ParteIsmael.java
+++ src/ParteIsmael.java
@@ -5,36 +5,64 @@
  */
 import java.util.Scanner;
 
+/*public class ParteIsmael {
+    public static void main(String[] args) {
+        int numFilas = 5; // Puedes ajustar este valor según tus necesidades
+        int longitudLinea = 2 * numFilas - 1;
+
+        for (int fila = 0; fila < numFilas; fila++) {
+            // Imprimir espacios en blanco
+            for (int espacioBlanco = 0; espacioBlanco < fila; espacioBlanco++) {
+                System.out.print("        "); // Ajusta el número de espacios según tu necesidad
+            }
+
+            // Imprimir el carácter "|"
+            System.out.print("|");
+
+            // Imprimir caracteres "_"
+            for (int i = 0; i < longitudLinea - 2 * fila; i++) {
+                System.out.print(" _");
+            }
+
+            System.out.println(); // Nueva línea para la siguiente fila
+        }
+    }
+}*/
+
 public class ParteIsmael {
-
-    /************************FIGURAS***************************/
-
-
     public static void main(String[] args) {
         System.out.println("Ingrese el numero de posiciones: ");
         Scanner sc = new Scanner(System.in);
         int nPosiciones = sc.nextInt();
-
         figura11(nPosiciones);
     }
-     
     /**F11:
             | _ 
                | _ _
                     | _ _ _
                            | _ _ _ _
                                     | _ _ _ _ _
-    * 
     */
-    
-    
-
-
     public static void figura11(int numFilas){
-        //String s = "|";
+        String vacio="  ";
+        for(int i=0 ; i<numFilas ; i++){
+            System.out.print("|");
+            for(int j=0 ; j<=i ; j++){
+                System.out.print("_");
+            }
+            System.out.println();
+            System.out.print(vacio);
+            vacio+=" ";
+        }
+    } 
+}
+    
+
+
+    /*public static void figura11(int numFilas){
         int espacios = numFilas;
         for(int filas=0 ; filas<numFilas ; filas++){
-            for(int espacioBlanco=0 ; espacioBlanco< espacios /*4*filas2*filas*/ ; espacioBlanco++){
+            for(int espacioBlanco=0 ; espacioBlanco< espacios  ; espacioBlanco++){
                 System.out.print(" ");
             }
             
@@ -50,5 +78,6 @@ public class ParteIsmael {
             variable++;
         }
 
-    }
-}
+    }*/
+
+/*4*filas2*filas*/
