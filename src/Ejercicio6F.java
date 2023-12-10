b4658386edfa69dbdec5bb1a720e0e1a785240c2
freddy1604 <freddyjimenez2003@gmail.com>
2023-12-10 17:11:49 -0500

    Update Ejercicio6F.java

--- src/Ejercicio6F.java
+++ src/Ejercicio6F.java
@@ -1,3 +1,10 @@
+/**
+*Copyright (C) 2K23, grupo2
+*(grupo2 - Opus Geeks), freddyjimenez2003@gmail.com
+*@Version 1.0.
+*@author: Freddy Jimenez.
+*FECHA: 11/12/2023.
+*/
 public class Ejercicio6F {
 
     public static void imprimirPatronAsteriscos(int numLineasFJ) {
