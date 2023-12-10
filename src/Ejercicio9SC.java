e4b60ce66fd307d6c1c4ae6581c20ab897ba6f78
freddy1604 <freddyjimenez2003@gmail.com>
2023-12-10 17:14:23 -0500

    Update Ejercicio9SC.java

--- src/Ejercicio9SC.java
+++ src/Ejercicio9SC.java
@@ -1,3 +1,10 @@
+/**
+*Copyright (C) 2K23, grupo2
+*(grupo2 - Opus Geeks), freddyjimenez2003@gmail.com
+*@Version 1.0.
+*@author: Freddy Jimenez.
+*FECHA: 11/12/2023.
+*/
 public class Ejercicio9SC {
     public void anS9C(int rep) {
         int cont1 = 0, cont2 = 1;
